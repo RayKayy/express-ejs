924cb1605715f6ea0628c5d45c80cee1d88b63ef
Raymond Kwan <rayk199492@gmail.com>
2018-09-02 19:26:30 +0000

    Added node_modules to  gitignore

--- app.js
+++ app.js
@@ -1,54 +1,55 @@
-const app = require("express")();
+const app = require('express')();
 
 const posts = [
   {
     id: 1,
     author: 'John',
     title: 'Templating with EJS',
-    body: 'Blog post number 1'
+    body: 'Blog post number 1',
   },
   {
     id: 2,
     author: 'Drake',
     title: 'Express: Starting from the Bottom',
-    body: 'Blog post number 2'
+    body: 'Blog post number 2',
   },
   {
     id: 3,
     author: 'Emma',
     title: 'Streams',
-    body: 'Blog post number 3'
+    body: 'Blog post number 3',
   },
   {
     id: 4,
     author: 'Cody',
     title: 'Events',
-    body: 'Blog post number 4'
+    body: 'Blog post number 4',
   },
   {
     id: 5,
     author: 'RayKayy',
     title: 'Test',
-    body: 'Testing for extra data'
-  }
-]
- app.set("view engine", "ejs");
+    body: 'Testing for extra data',
+  },
+];
 
- app.get('/', (req, res) => {
-   res.render('home', {posts: posts});
- })
+app.set('view engine', 'ejs');
 
- app.get('/post/:id', (req, res) => {
-   const post = posts.filter((post) => {
-     return post.id == req.params.id;
-   })[0];
-   res.render('post', {
-     author: post.author,
-     title: post.title,
-     body: post.body,
-   });
+app.get('/', (req, res) => {
+  res.render('home', { posts });
+});
+
+app.get('/post/:id', (req, res) => {
+  const post = posts.filter((obj) => {
+    return obj.id === Number(req.params.id);
+  })[0];
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body,
   });
+});
 
- app.listen(8080);
+app.listen(8080);
 
- console.log("listening on port 8080");
+console.log('listening on port 8080');
